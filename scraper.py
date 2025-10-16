41b17e42b79888ee10f3ad45a957e2ef69358448
Monobola <nunom3ndes2005@gmail.com>
2025-10-16 17:30:59 +0100

    headless option adicionano main domain adicionado

--- scraper.py
+++ scraper.py
@@ -14,7 +14,22 @@ nmr_pagina= 3
 #Nome da pasta onde salvar
 folder_name= "scraper_download"
 
+#main domain
+main_domain="https://www.vecteezy.com"
+
 #cria pasta para donwload usar o OS para criar e prcourar paasta
 if not os.path.exists(folder_name):
     os.mkdir(folder_name)
-    print(f"Pasta {folder_name} criada")
+    print(f"Folder {folder_name} created")
+
+
+#modo headless sem abrir pagina google descomentar 
+#options = webdriver.ChoromeOptions()
+#options.add_argument("--headless")
+#driver =webdriver.Chorome(options=options)
+
+#start selenium drivers para abrir uma pagina no chorome
+driver = webdriver.Chorome()
+wait = WebDriverWait(driver, 15)#define 15s para a pagina carregar toda
+
+print("google paga open")
