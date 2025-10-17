f2dcdd02d7307c9804584fc364e5d7dca9e02e31
Monobola <nunom3ndes2005@gmail.com>
2025-10-17 12:00:42 +0100

    loop para procurar o linkd com as paginas de download fazer a procura no numero de paginas escolhida
    gitignore para nao push o chorome driver para o github

--- scraper.py
+++ scraper.py
@@ -33,3 +33,43 @@ driver = webdriver.Chorome()
 wait = WebDriverWait(driver, 15)#define 15s para a pagina carregar toda
 
 print("google page open")
+
+#salvar o link the todos os assets 
+link_assets=[]
+
+#loop para a pagina 1 ate a pagina defenida 
+for page_num in range(1, nmr_pagina +1):
+    #monta o url da pagina com o base mais o endereço da pagina e numero 
+    url_pag= f"{base_url}&page={page_num}"
+    print(f"Processing page {page_num} with url: {url_pag}")
+
+    #driver vai dar start na pagina
+    driver.get(url_pag)
+
+    try:
+        #esperar que a pagina carregue com a ajuda da div "ul.ez-resource-grid--main-grid"
+        #garante que a pagina esta aberta
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.ez-resource-grid--main-grid")))
+        time.sleep(2)#pausa para garantir que tudo abriu 
+
+        #encontrar os elementos que contem o link 
+        # selector "li.ez-resource-grid__item a" vai procurar as tag dentro das tags li com a class ez-resource-grid__item
+        link_element= driver.find_elements(By.CSS_SELECTOR, "li.ez-resource-grid__item a")
+
+        #para cada elemento encontrado vamos contruir o url completo, isto ira criar os url estao vai estar os botoes de downlaod 
+        for elemento in link_element:
+            link_relativo= elemento.get_attribute('href')
+
+            #verifacar se contem link
+            if link_relativo:
+                #juntar o dominio mais o link para o link completo
+                link_final=f"{main_domain}{link_relativo}"
+
+                #adiconar ao dicionario apenas se nao estiver 
+                if link_final not in link_assets:
+                    link_assets.append(link_final)
+
+    except Exception as e:
+        print(f"Err procecing page num {page_num} err = {e}")
+    
+print(f"\nFind {len(link_assets)} link assets for donwload.")
