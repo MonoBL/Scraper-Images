fce8db7770cc53187a8a756295d10b4747076de8
Monobola <nunom3ndes2005@gmail.com>
2025-10-22 10:55:36 +0100

    headless mode

--- scraper.py
+++ scraper.py
@@ -42,9 +42,9 @@ def run_scraper(base_url, nmr_pagina, job_state):
     }
 
     chrome_options.add_experimental_option("prefs",prefs)
-    #chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
-    #chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")  # ← NOVO: User agent
